9b6c1111b2691a5930280c63f1dcc94112166013
Himagiri232 <66547635+Himagiri232@users.noreply.github.com>
2020-06-20 10:26:58 +0530

    for utility grouped

---
+++ src/views/UtilityDeity.vue
@@ -0,0 +1,124 @@
+<template>
+<div >
+    <div >
+      <p>
+        <label v-if="isSuccess" class="alert alert-success" role="alert">{{getMessage}}</label>
+        <label v-else-if="getMessage" class="alert alert-danger" role="alert">{{getMessage}}</label>
+      </p>
+    </div>
+   
+    <div>
+      <b-card no-body>
+        
+          <b-tabs active-nav-item-class="font-weight-bold"
+            content-class="mt-3">
+            <b-tab v-for="tab in tabs" 
+              :key="tab.id" 
+              :title="tab.title" 
+              @click="selectedComponent = tab.selectedComponent">
+              <div >
+                <!-- <p>{{ selectedComponent }}</p> -->
+                <keep-alive>
+                  <component :is="selectedComponent" >
+                    <p>Default Content</p>
+                  </component>
+                </keep-alive>
+              </div>
+            </b-tab>
+          </b-tabs>
+      </b-card>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import UKulam from '@/components/kulam/UKulam.vue';
+import UGothiram from '@/components/gothiram/UGothiram.vue';
+import UCaste from '@/components/caste/UCaste.vue';
+import UDeity from '@/components/deity/UDeity.vue';
+
+export default {
+  name: 'Util',
+  data: function() {
+    return {
+      kulamTitle: 'Kulam',
+      selectedComponent: 'appKulam',
+      
+tabs: [
+        {
+          id: 'kulam',
+          title: 'Kulam',
+          selectedComponent: 'appKulam'
+        },
+        {
+          id: 'gothiram',
+          title: 'Gothiram',
+          selectedComponent: 'appGothiram'
+        },
+        {
+          id: 'caste',
+          title: 'Caste',
+          selectedComponent: 'appCaste'
+        },
+        {
+          id: 'deity',
+          title: 'Deity',
+          selectedComponent: 'appDeity'
+        },
+
+        ]
+
+    };
+  },
+  computed: {
+    ...mapGetters(['getMessage', 'isSuccess'])
+  },
+  components: {
+    appKulam: UKulam,
+    appGothiram: UGothiram,
+    appCaste: UCaste,
+    appDeity: UDeity,
+
+  }
+};
+</script>   
+
+<style scoped>
+.entry-container.card {
+  width: 70%;
+  height:100%;
+  float: left;
+  padding: 70px;
+  border-style: none !important;
+  position: absolute;
+}
+
+.utilitydiv.card {
+width: 70%; 
+height:80%;  
+border: 1px ;
+solid:black; 
+position: absolute;
+} 
+
+.parent{
+  display: flex;
+  flex-direction: column;
+   align-items: center;
+  flex-wrap: nowrap;
+    height: 100%;
+}
+
+
+.child{
+  width:100%;
+ 
+  
+  height: 100%;
+    flex: 0 1;
+  
+    padding:5px;
+}
+</style>
---
+++ src/views/UtilityProfession.vue
@@ -0,0 +1,131 @@
+<template>
+<div >
+    <div >
+      <p>
+        <label v-if="isSuccess" class="alert alert-success" role="alert">{{getMessage}}</label>
+        <label v-else-if="getMessage" class="alert alert-danger" role="alert">{{getMessage}}</label>
+      </p>
+    </div>
+   
+    <div>
+      <b-card no-body>
+        
+          <b-tabs active-nav-item-class="font-weight-bold"
+            content-class="mt-3">
+            <b-tab v-for="tab in tabs" 
+              :key="tab.id" 
+              :title="tab.title" 
+              @click="selectedComponent = tab.selectedComponent">
+              <div >
+                <!-- <p>{{ selectedComponent }}</p> -->
+                <keep-alive>
+                  <component :is="selectedComponent" >
+                    <p>Default Content</p>
+                  </component>
+                </keep-alive>
+              </div>
+            </b-tab>
+          </b-tabs>
+      </b-card>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import UEducation from '@/components/education/UEducation.vue';
+import UOccupation from '@/components/occupation/UOccupation.vue';
+import UJobtype from '@/components/jobtype/UJobtype.vue';
+import UBusinesstype from '@/components/businesstype/UBusinesstype.vue';
+import UCourse from '@/components/course/UCourse.vue';
+
+export default {
+  name: 'Util',
+  data: function() {
+    return {
+      kulamTitle: 'Education',
+      selectedComponent: 'appEducation',
+      
+tabs: [
+        {
+          id: 'education',
+          title: 'Education',
+          selectedComponent: 'appEducation'
+        },
+        {
+          id: 'occupation',
+          title: 'Occupation',
+          selectedComponent: 'appOccupation'
+        },
+        {
+          id: 'jobtype',
+          title: 'Jobtype',
+          selectedComponent: 'appJobtype'
+        },
+        {
+          id: 'businessType',
+          title: 'BusinessType',
+          selectedComponent: 'appBusinessType'
+        },
+        {
+          id: 'course',
+          title: 'Course',
+          selectedComponent: 'appCourse'
+        },
+        ]
+
+    };
+  },
+  computed: {
+    ...mapGetters(['getMessage', 'isSuccess'])
+  },
+  components: {
+    appEducation: UEducation,
+    appJobtype: UJobtype,
+    appCourse: UCourse,
+    appOccupation: UOccupation,
+    appJobtype: UJobtype,
+    appBusinesstype: UBusinesstype,
+
+  }
+};
+</script>   
+
+<style scoped>
+.entry-container.card {
+  width: 70%;
+  height:100%;
+  float: left;
+  padding: 70px;
+  border-style: none !important;
+  position: absolute;
+}
+
+.utilitydiv.card {
+width: 70%; 
+height:80%;  
+border: 1px ;
+solid:black; 
+position: absolute;
+} 
+
+.parent{
+  display: flex;
+  flex-direction: column;
+   align-items: center;
+  flex-wrap: nowrap;
+    height: 100%;
+}
+
+
+.child{
+  width:100%;
+ 
+  
+  height: 100%;
+    flex: 0 1;
+  
+    padding:5px;
+}
+</style>
